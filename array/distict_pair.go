043ec22a42670143e3d6e8b63ecddd261dbdbb5e
tangguhriyadi <mtangguh97@gmail.com>
2023-08-24 10:24:19 +0700

    refactor distinct pair

--- array/distict_pair.go
+++ array/distict_pair.go
@@ -1,21 +1,21 @@
 package array
 
-import "fmt"
-
 func CountDistinctPairsWithSum(arr []int, target int) int {
 	pairCount := 0
 
-	fmt.Println(len(arr))
-
 	for i := 0; i < len(arr); i++ {
 		for j := i + 1; j < len(arr); j++ {
-			count := fmt.Sprintf("counting %d + %d", arr[i], arr[j])
-			fmt.Println(count)
 			if arr[j]+arr[i] == target {
 				pairCount++
 			}
 		}
 	}
+
+	// for i, j := 0, 1; i < len(arr) && j < len(arr); i, j = i+1, j+1 {
+	// 	if arr[j]+arr[i] == target {
+	// 		pairCount++
+	// 	}
+	// }
 
 	return pairCount
 
